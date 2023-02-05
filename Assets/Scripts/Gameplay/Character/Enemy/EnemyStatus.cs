60362d781db2862dc5536f35323a9e3435d7afbb
nyarla <nyarladev@gmail.com>
2023-02-05 13:48:47 +0300

    Enemy damage and death vfx and sound

--- Assets/Scripts/Gameplay/Character/Enemy/EnemyStatus.cs
+++ Assets/Scripts/Gameplay/Character/Enemy/EnemyStatus.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Extentions;
+using Extentions.Factory;
 using Gameplay.UI;
 using UnityEngine;
 using Zenject;
@@ -8,18 +9,29 @@ namespace Gameplay.Character.Enemy
 {
     public class EnemyStatus : LazyGetComponent<EnemyComposition>
     {
+        [SerializeField] private GameObject _damagePrefab;
+        [SerializeField] private GameObject _deathPrefab;
         [SerializeField] private GameObject _healthbarPrefab;
         [SerializeField] private Transform _healthbarOrigin;
 
+        [Inject] private ContainerFactory Factory { get; set; }
+        
         public RectTransform HUD { get; set; }
         
         private void Awake()
         {
             Lazy.VitalsPool.HealthIsOver += Die;
+            Lazy.VitalsPool.TookDamage += PlayDamageEffect;
+        }
+
+        private void PlayDamageEffect(float _)
+        {
+            Factory.Instantiate<Transform>(_damagePrefab, Transform.position.WithY(1.5f));
         }
 
         private void Start()
         {
+            Factory.Instantiate<Transform>(_deathPrefab, Transform.position.WithY(1.5f));
             FloatingHealthbar healthbar = Instantiate(_healthbarPrefab, HUD).GetComponent<FloatingHealthbar>();
             healthbar.Init(Lazy.VitalsPool.Health);
             healthbar.InitFloating(Camera.main, _healthbarOrigin, Lazy.VitalsPool);
